373642786320218d42374809608422b37c980d9b
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-01-27 12:51:52 -0600

    removed warning

--- Assets/Scripts/CDEPMeshGeneration.cs
+++ Assets/Scripts/CDEPMeshGeneration.cs
@@ -12,21 +12,21 @@ public class CDEPMeshGeneration : MeshGeneration
      
     public void SetCamPos(Vector3 cameraPos)
     {
-        renderer.material.SetVector("_camera_position", cameraPos);
+        meshRenderer.material.SetVector("_camera_position", cameraPos);
     }
 
     public void SetCamDirection(Vector4 cameraDir)
     {
-        renderer.material.SetVector("_xr_view_dir", cameraDir);
+        meshRenderer.material.SetVector("_xr_view_dir", cameraDir);
     }
 
     public void SetAspect(float aspectRatio)
     {
-        renderer.material.SetFloat("_xr_aspect", aspectRatio);
+        meshRenderer.material.SetFloat("_xr_aspect", aspectRatio);
     }
 
     public void SetCameraIndex(int idx)
     {
-        renderer.material.SetFloat("_img_index", idx);
+        meshRenderer.material.SetFloat("_img_index", idx);
     }
 }
